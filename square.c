846f5b839962c93aaf674eab8f34cb8abdd40831
Aniket Malvade <malvade3000@gmail.com>
2016-01-20 19:49:29 +0530

    Square calculation

---
+++ square.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, square, cube;
+
+	a = 17;
+
+	square = a * a ;
+	cube = a * a * a;
+
+	printf("Square of a(%d)= %d\n", a, square); 
+	
+	printf("cube of a(%d)= %d\n", a, cube );	
+}
